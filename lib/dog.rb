aee091bd4f5d546e92273e87c286b9fb6de9d3f7
popechrisj@gmail.com <popechrisj@gmail.com>
2018-05-24 14:27:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
-  def name(name)
-    @name = name
+  def name()
+    @name
   end
   def name=(name)
     @name = name
   end
-  def breed(breed)
-    @breed = breed
+  def breed()
+    @breed
   end
   def breed=(breed)
     @breed = breed
